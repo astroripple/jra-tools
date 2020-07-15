9754a9e9812daaf8c0d4b3e9b4ab336db0886f55
Yuma Iwasaki <requrd1989@gmail.com>
2020-07-15 23:33:28 +0900

    Fix typo

--- performance_tester.py
+++ performance_tester.py
@@ -6,7 +6,7 @@ from .label_creator import LabelCreator
 class PerformanceTester:
     def __init__(self, model_name, kaisais):
         #モデルのロード
-        self.model = load_model(modelname)
+        self.model = load_model(model_name)
         self.recoveries = get_recoveries(kaisais)
 
     def output_performance():
