656e231bb047e89f58d6822f1ae24ee13f98da3f
Yuma Iwasaki <requrd1989@gmail.com>
2020-07-17 00:28:33 +0900

    Fix date checker

--- kaisai_creator.py
+++ kaisai_creator.py
@@ -2,4 +2,4 @@ from sqlalchemy.orm import joinedload
 from horseview.horsemodel import KaisaiData
 
 def get_kaisais(start, end):
-    return KaisaiData.query.filter(KaisaiData.ymd <= start,KaisaiData.ymd >=end).options(joinedload('*')).all()
+    return KaisaiData.query.filter(KaisaiData.ymd >= start,KaisaiData.ymd <=end).options(joinedload('*')).all()
