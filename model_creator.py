f7f831fb44566486c8d2bd2fa39217dade658892
Yuma Iwasaki <requrd1989@gmail.com>
2020-09-25 08:44:09 +0900

    Refactor model creator

--- model_creator.py
+++ model_creator.py
@@ -5,55 +5,32 @@ class ModelCreator:
     def __init__(self, num_trait):
         in_ = Input((18, num_trait))
         merged = Conv1D(filters=128, kernel_size=1, padding="same")(in_)
-        merged = GaussianNoise(.5)(in_)
 
-        in_conv = Conv1D(filters=128, kernel_size=3, padding="same")(merged)
-        in_conv_conv = Conv1D(
-            filters=128, kernel_size=3, padding="same", activation="selu"
-        )(in_conv)
-        merged = Add()([in_conv_conv, in_conv])
-        merged = Activation("selu")(merged)
-        
-        in_conv = Conv1D(filters=128, kernel_size=3, padding="same")(merged)
-        in_conv_conv = Conv1D(
-            filters=128, kernel_size=3, padding="same", activation="selu"
-        )(in_conv)
-        merged = Add()([in_conv_conv, in_conv])
-        merged = Activation("selu")(merged)
-       
-        in_conv = Conv1D(filters=128, kernel_size=3, padding="same")(merged)
-        in_conv_conv = Conv1D(
-            filters=128, kernel_size=3, padding="same", activation="selu"
-        )(in_conv)
-        merged = Add()([in_conv_conv, in_conv])
-        merged = Activation("selu")(merged)
+        merged = self.middle_layer(merged)
+        merged = self.middle_layer(merged)
+        merged = self.middle_layer(merged)
         merged = Dropout(.5)(merged)
 
         # 出力層
-        out1 = Conv1D(filters=1, kernel_size=1)(merged)
-        out1 = Flatten()(out1)
-        out1 = Activation("softmax")(out1)
-
-        out2 = Conv1D(filters=1, kernel_size=1)(merged)
-        out2 = Flatten()(out2)
-        out2 = Activation("softmax")(out2)
-
-        out3 = Conv1D(filters=1, kernel_size=1)(merged)
-        out3 = Flatten()(out3)
-        out3 = Activation("softmax")(out3)
-
-        out4 = Conv1D(filters=1, kernel_size=1)(merged)
-        out4 = Flatten()(out4)
-        out4 = Activation("softmax")(out4)
-
-        out5 = Conv1D(filters=1, kernel_size=1)(merged)
-        out5 = Flatten()(out5)
-        out5 = Activation("softmax")(out5)
+        output_layers = [self.output_layer(merged) for i in range(5)]
 
         # コンパイル
-        model = Model(inputs=in_, outputs=[out1, out2, out3, out4, out5])
+        model = Model(inputs=in_, outputs=output_layers)
         model.compile(
             loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"]
         )
         self.model = model
+
+    def output_layer(self, input_layer):
+        out = Conv1D(filters=1, kernel_size=1)(input_layer)
+        out = Flatten()(out)
+        return Activation("softmax")(out)
+
+    def middle_layer(self, input_layer):
+        in_conv = Conv1D(filters=128, kernel_size=3, padding="same")(input_layer)
+        in_conv_conv = Conv1D(
+            filters=128, kernel_size=3, padding="same", activation="selu"
+        )(in_conv)
+        merged = Add()([in_conv_conv, in_conv])
+        return Activation("selu")(merged)
         
