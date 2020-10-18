897ca4d62ccaae6f883076a65dcba342af0c7b1b
Yuma Iwasaki <requrd1989@gmail.com>
2020-10-18 17:24:41 +0900

    Add bao importer class

--- bao_importer.py
+++ bao_importer.py
@@ -1,36 +1,68 @@
-from horseview.horsemodel import sesobj, KaisaiData, UmarenOddsData
+from horseview.horsemodel import sesobj, UmarenOddsData
 
-def getKaisaiInfo(kaisaikey):
-    return {
-        "ba" :kaisaikey[0:2],
-        "year" :kaisaikey[2:4],
-        "kai" :kaisaikey[4:5],
-        "day" :kaisaikey[5:6]
-    }
+class BaoImporter:
+    def __init__(self, kaisais):
+        self.kaisaikeys = [
+            {
+                "course": self.getCourseCode(kaisai.kaisaikey),
+                "ymd": kaisai.ymd,
+                "key": kaisai.kaisaikey
+            } for kaisai in kaisais
+        ]
 
-def getCourseCode(kaisaikey):
-    return kaisaikey[0:2]
-
-def getRaceKey(bao_info):
-    if kaisaikeys[0]["ymd"] <= bao_info["ymd"] <= kaisaikeys[-1]["ymd"]:
-        for key in kaisaikeys:
-            if key["ymd"] == bao_info["ymd"] and key["course"] == bao_info["bacode"]:
-                return key["key"] + bao_info["no"]
-
-def getBaoCodeInfo(race_code):
-        year_header = '19' if race_code[0] == '1' else '20'
+    def getKaisaiInfo(self, kaisaikey):
         return {
-            'year': year_header + race_code[1:3],
-            'month': race_code[3:5],
-            'day': race_code[5:7],
-            'ymd': int(year_header + race_code[1:7]),
-            'bacode': race_code[7:9],
-            'no': race_code[9:11]
+            "ba" :kaisaikey[0:2],
+            "year" :kaisaikey[2:4],
+            "kai" :kaisaikey[4:5],
+            "day" :kaisaikey[5:6]
         }
 
-def getUmarenOdds(odds):
-    odds_dict = {}
-    for i, odd_str in enumerate(odds):
-        for j in range(0, len(odd_str), 6):
-            odds_dict.update({f'{i+1}-{i+int((j+6)/6)+1}': int(odd_str[j:j+6])/10})
-    return odds_dict
+    def getCourseCode(self, kaisaikey):
+        return kaisaikey[0:2]
+
+    def getRaceKey(self, bao_info):
+        if self.kaisaikeys[0]["ymd"] <= bao_info["ymd"] <= kaisaikeys[-1]["ymd"]:
+            for key in kaisaikeys:
+                if key["ymd"] == bao_info["ymd"] and key["course"] == bao_info["bacode"]:
+                    return key["key"] + bao_info["no"]
+
+    def getBaoCodeInfo(self, race_code):
+            year_header = '19' if race_code[0] == '1' else '20'
+            return {
+                'year': year_header + race_code[1:3],
+                'month': race_code[3:5],
+                'day': race_code[5:7],
+                'ymd': int(year_header + race_code[1:7]),
+                'bacode': race_code[7:9],
+                'no': race_code[9:11]
+            }
+
+    def getUmarenOdds(self, odds):
+        odds_dict = {}
+        for i, odd_str in enumerate(odds):
+            for j in range(0, len(odd_str), 6):
+                odds_dict.update({f'{i+1}-{i+int((j+6)/6)+1}': int(odd_str[j:j+6])/10})
+        return odds_dict
+
+    def importUmarenCsv(self):
+        with open('umaren_odds.csv') as f:
+            reader = csv.reader(f)
+            header = next(reader)
+            for row in reader:
+                race_code = str(int(float(row[2])))
+                bao_info = getBaoCodeInfo(race_code)
+                racekey = getRaceKey(bao_info)
+                if racekey:
+                    odd = UmarenOddsData(
+                        racekey=racekey,
+                        data_kbn=row[0],
+                        registered_horses=row[4],
+                        ran_horses=row[5],
+                        sold_flg=row[6],
+                        all_odds=getOdds(row[7].split()),
+                        all_estimated_odds=row[8],
+                        sum_of_all_bought_count=row[9]
+                    )
+                    sesobj.add(odd)
+            sesobj.commit()
