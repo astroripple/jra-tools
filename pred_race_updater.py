27aac93321467484ec5a8d8d6a3a0c2b8ad730ae
Yuma Iwasaki <requrd1989@gmail.com>
2020-10-29 23:42:31 +0900

    Add wide odds functions

--- pred_race_updater.py
+++ pred_race_updater.py
@@ -27,3 +27,18 @@ class PredictRaceUpdater:
         p = horse1.predict.pp_icchaku * horse2.predict.pp_nichaku + horse1.predict.pp_nichaku * horse2.predict.pp_icchaku
         return 1/p
 
+    def wideOddses(self, race):
+        odds_dict = {}
+        for i, horse1 in enumrate(race.racehorses):
+            for j in range(i+1, len(race.racehorses)):
+                horse2 = race.racehorses[j]
+                odds_dict.update({
+                    f"{horse1.num}-{horse2.num}": self.wideOdds(horse1, horse2)
+                })
+        return odds_dict
+
+    def wideOdds(self, horse1, horse2):
+        p = horse1.predict.pp_icchaku * horse2.predict.pp_nichaku + horse1.predict.pp_icchaku * horse2.predict.pp_sanchaku
+        p += horse1.predict.pp_nichaku * horse2.predict.pp_icchaku + horse1.predict.pp_nichaku * horse2.predict.pp_sanchaku
+        p += horse1.predict.pp_sanchaku * horse2.predict.pp_icchaku + horse1.predict.pp_sanchaku * horse2.predict.pp_icchaku
+        return 1/p
