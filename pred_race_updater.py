efea86f0e1e635223c36d0c77c73e9df7896f0af
Yuma Iwasaki <requrd1989@gmail.com>
2020-10-30 00:29:28 +0900

    Add wakuren odds methods

--- pred_race_updater.py
+++ pred_race_updater.py
@@ -55,3 +55,35 @@ class PredictRaceUpdater:
             + horse1.predict.pp_sanchaku * horse2.predict.pp_icchaku
         )
         return 1 / p
+
+    def wakurenOddses(self, race):
+        odds_dict = {}
+        for i in range(8):
+            for j in range(i, 8):
+                gate1 = i + 1
+                gate2 = j + 1
+                odds_dict.update(
+                    {f"{gate1}-{gate2}": self.wakurenOdds(race, gate1, gate2)}
+                )
+        return odds_dict
+
+    def wakurenOdds(self, race, gate1, gate2):
+        horses1 = [horse for horse in race.racehorses if horse.waku == gate1]
+        if gate1 == gate2:
+            return self.wakurenSameOdds(horses1)
+        horses2 = [horse for horse in race.racehorses if horse.waku == gate2]
+        pp = 0
+        for horse1 in horses1:
+            for horse2 in horses2:
+                pp += 1 / self.umarenOdds(horse1, horse2)
+        return 1 / pp
+
+    def wakurenSameOdds(self, horses):
+        if len(horses) <= 1:
+            return 0
+        pp = 0
+        for i, horse1 in enumrate(horses):
+            for j in range(i + 1, len(horses)):
+                horse2 = horses[j]
+                pp += 1 / self.umarenOdds(horse1, horse2)
+        return 1 / pp
