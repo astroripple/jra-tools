aa20ca7f8146975a17ebf36e12d43bf53a8ca027
Yuma <requrd1989@gmail.com>
2022-06-03 00:52:34 +0900

    Add init py

---
+++ jra_tools/__init__.py
@@ -0,0 +1,16 @@
+from bao_importer import *
+from category_data import *
+from db_checker import *
+from input_creator import *
+from jrdbdummies import *
+from kaisai_creator import *
+from label_creator import *
+from labelutil import *
+from model_creator import *
+from performance_tester import *
+from pred_race_updater import *
+from schedule import *
+from training_tool import *
+from trainning_visualizer import *
+
+__version__ = '1.0.0'
