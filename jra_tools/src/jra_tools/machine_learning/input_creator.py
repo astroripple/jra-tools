88af522cbe1d8b6f24e68252abd4818281e630d0
Yuma <requrd1989@gmail.com>
2023-07-11 10:12:59 +0000

    Add pydoc

--- jra_tools/src/jra_tools/machine_learning/input_creator.py
+++ jra_tools/src/jra_tools/machine_learning/input_creator.py
@@ -1,10 +1,13 @@
-﻿import numpy as np
+﻿"""推論に必要なデータセットを作成する"""
+import numpy as np
 from jra_tools import KaisaiData
 from . import training_tool
 from . import category_data
 
 
 class InputCreator:
+    """KaisaiDataを元にndarrayを作成する"""
+
     def __init__(self, kaisais: list[KaisaiData]):
         score_data = training_tool.createScoreDataMatrix(kaisais)
         sds = training_tool.standardize(score_data)
