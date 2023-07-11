3e8f2c4758974d74a7a6d6defc83d9e102dae2d1
Yuma <requrd1989@gmail.com>
2023-07-11 10:28:22 +0000

    Add return value types

--- jra_tools/src/jra_tools/machine_learning/category_data.py
+++ jra_tools/src/jra_tools/machine_learning/category_data.py
@@ -4,12 +4,12 @@ from jrdb_model import KaisaiData, BangumiData, RacehorseData
 from .jrdbdummies import CategoryGetter
 
 
-def getCategoryData(kaisais: list[KaisaiData]):
+def getCategoryData(kaisais: list[KaisaiData]) -> np.ndarray:
     categories = _getCategories(kaisais)
     return _convertToMatrix(categories)
 
 
-def _convertToMatrix(categories):
+def _convertToMatrix(categories) -> np.ndarray:
     matrix = np.zeros([len(categories), 18, len(categories[0][0])])
     for raceNum in range(len(categories)):
         for horseNum in range(len(categories[raceNum])):
@@ -17,13 +17,13 @@ def _convertToMatrix(categories):
     return matrix
 
 
-def _getCategories(kaisais: list[KaisaiData]):
+def _getCategories(kaisais: list[KaisaiData]) -> list[np.ndarray]:
     return [
         _getRaceCategories(kaisai, race) for kaisai in kaisais for race in kaisai.races
     ]
 
 
-def _getRaceCategories(kaisai: KaisaiData, race: BangumiData):
+def _getRaceCategories(kaisai: KaisaiData, race: BangumiData) -> np.ndarray:
     horses = sorted(race.racehorses, key=lambda h: h.racehorsekey)
     for horse in horses:
         if horse.num == 1:
@@ -33,7 +33,9 @@ def _getRaceCategories(kaisai: KaisaiData, race: BangumiData):
     return dummies
 
 
-def _getCategory(kaisai: KaisaiData, race: BangumiData, horse: RacehorseData):
+def _getCategory(
+    kaisai: KaisaiData, race: BangumiData, horse: RacehorseData
+) -> np.ndarray:
     cg = CategoryGetter()
     return np.hstack(
         (
