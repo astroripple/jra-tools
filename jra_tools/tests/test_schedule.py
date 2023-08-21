112e50fdf80ea7d67b424be2e8b443199b8a5772
Yuma <requrd1989@gmail.com>
2023-08-21 10:40:52 +0000

    Fix pytest for methods

--- jra_tools/tests/test_schedule.py
+++ jra_tools/tests/test_schedule.py
@@ -1,21 +1,49 @@
 """Yahoo競馬のクローラーテスト"""
 import pickle
+from pytest import fixture
 from pytest_mock import MockFixture
-from src.jra_tools.database.schedule import annual_schedule
+from src.jra_tools.database.schedule import annual_schedule, open_days
 
 
-def test_annual_schedule(mocker: MockFixture):
-    """スケジューラをテストする
+@fixture
+def patched_get(mocker: MockFixture):
+    """requestsモジュールをパッチしておく
 
     Args:
-        mocker (MockFixture): モッカーオブジェクト
+        mocker (MockFixture): モッカー
+
+    Yields:
+        _type_: request.get
     """
     patched_requests = mocker.patch("src.jra_tools.database.schedule.requests")
     patched_get = patched_requests.get
     mock_response = patched_get.return_value
     with open("jra_tools/tests/yahoo_keiba.pkl", mode="rb") as f:
         mock_response.text = pickle.load(f)
-        # print(mock_response.text)
+    yield patched_get
+
+
+def test_open_days(patched_get):
+    """_summary_
+
+    Args:
+        patched_get (_type_): _description_
+    """
+    days = open_days(1, 2018)
+
+    assert patched_get.call_args_list[0][0] == (
+        "https://keiba.yahoo.co.jp/schedule/list/2018/",
+        {"month": "1"},
+    )
+    assert len(days) == 8
+
+
+def test_annual_schedule(patched_get):
+    """スケジューラをテストする
+
+    Args:
+        mocker (MockFixture): モッカーオブジェクト
+    """
 
     days = annual_schedule(2018)
 
@@ -26,5 +54,5 @@ def test_annual_schedule(mocker: MockFixture):
     )
     # assert len(days) == 109
     # assert days[0] == 20180106
-    # assert days[-1] == 20181228
+    assert days[-1] == 20181228
     # assert days[54] == 20180701
