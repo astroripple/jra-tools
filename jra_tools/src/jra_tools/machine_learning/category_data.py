97eaca0ee699873c8f6f01dd5bb41b68a1493783
Yuma <requrd1989@gmail.com>
2023-08-31 05:58:48 +0000

    remove unnecessary fields

--- jra_tools/src/jra_tools/machine_learning/category_data.py
+++ jra_tools/src/jra_tools/machine_learning/category_data.py
@@ -47,18 +47,7 @@ def _get_category(
     kaisai: KaisaiData, race: BangumiData, horse: RacehorseData
 ) -> np.ndarray:
     cg = CategoryGetter()
-    return np.hstack(
-        (
-            cg.getTennatsu(kaisai.tennatsu),
-            cg.getDistance(race.distance),
-            cg.getBacode(horse.bacode),
-            cg.getNum(horse.num),
-            cg.getWaku(horse.waku),
-            cg.getTorikeshi(_filter_str_to_int(horse.torikeshi)),
-            cg.getBanushikaicode(_filter_str_to_int(horse.banushikai_code)),
-            cg.getTraintype(_filter_str_to_int(horse.trainanalysis.train_type)),
-        )
-    )
+    return np.hstack((cg.getWaku(horse.waku),))
 
 
 def _filter_str_to_int(value):
