cc1b570c62e565a282599062566195bcb3c92138
Yuma Iwasaki <requrd1989@gmail.com>
2024-05-01 04:01:55 +0000

    add test kaisai loader

--- jra_tools/tests/test_kaisai_loader.py
+++ jra_tools/tests/test_kaisai_loader.py
@@ -1,6 +1,13 @@
 """開催データ大量ロードモジュールのユニットテスト"""
 
+from pytest_mock import MockerFixture
 
-def test_load():
+
+def test_load(mocker: MockerFixture):
     """get_kaisaisを複数回に分けで呼び出すことを確認する"""
-    pass
+    mock_kaisais = mocker.patch(
+        "src.jra_tools.machine_learning.kaisai_loader.get_kaisais"
+    )
+
+    for call in mock_kaisais.mock_calls:
+        assert call.call_list == []
