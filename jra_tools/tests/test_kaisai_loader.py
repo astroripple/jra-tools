548c641838913e9e95b97600add239ade6050761
Yuma Iwasaki <requrd1989@gmail.com>
2024-05-01 07:59:04 +0000

    Fix assert

--- jra_tools/tests/test_kaisai_loader.py
+++ jra_tools/tests/test_kaisai_loader.py
@@ -5,9 +5,19 @@ from pytest_mock import MockerFixture
 
 def test_load(mocker: MockerFixture):
     """get_kaisaisを複数回に分けで呼び出すことを確認する"""
-    mock_kaisais = mocker.patch(
-        "src.jra_tools.machine_learning.kaisai_loader.get_kaisais"
+    mock_get_kaisais = mocker.patch(
+        "jra_tools.machine_learning.kaisai_loader.get_kaisais"
     )
+    from jra_tools.machine_learning.kaisai_loader import load
 
-    for call in mock_kaisais.mock_calls:
-        assert call.call_list == []
+    load(20120101, 20151231)
+
+    expected_args = (
+        (20120101, 20121231),
+        (20130101, 20131231),
+        (20140101, 20141231),
+        (20150101, 20151231),
+    )
+    assert mock_get_kaisais.call_count == 4
+    for call, args in zip(mock_get_kaisais.call_args_list, expected_args):
+        assert call.args == args
