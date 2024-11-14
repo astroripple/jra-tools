ed5e759cb4fbc7c5dfe521ab8c83dd931bfa93c6
Yuma Iwasaki <requrd1989@gmail.com>
2024-11-14 05:54:58 +0000

    add save method

--- src/jra_tools/machine_learning/input_creator.py
+++ src/jra_tools/machine_learning/input_creator.py
@@ -1,4 +1,5 @@
 ﻿"""推論に必要なデータセットを作成する"""
+
 from typing import List
 import numpy as np
 from jrdb_model import KaisaiData
@@ -14,3 +15,12 @@ class InputCreator:
         sds = training_tool.standardize(score_data)
         cd = category_data.get_category_data(kaisais)
         self.x_data = np.concatenate((sds, cd), axis=2)
+
+    def save(self, name: str) -> None:
+        """入力データを永続化する
+
+        Args:
+            name (str): ファイル名。拡張子も含めること。
+        """
+        with open(name, mode="wb") as f:
+            self.x_data.dump(f)
