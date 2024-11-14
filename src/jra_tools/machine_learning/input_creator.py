fae5a1874da12a858c2a14b16cc09e980ee52aa8
Yuma Iwasaki <requrd1989@gmail.com>
2024-11-14 06:18:07 +0000

    add create input data

--- src/jra_tools/machine_learning/input_creator.py
+++ src/jra_tools/machine_learning/input_creator.py
@@ -24,3 +24,26 @@ class InputCreator:
         """
         with open(name, mode="wb") as f:
             self.x_data.dump(f)
+
+
+def create_training_input_data(start: int, end: int):
+    """指定した期間のデータセットを作成する
+
+    Args:
+        start (int): YYYY
+        end (int): YYYY
+
+    Raises:
+        RuntimeError: データのロードまたは保存中にエラーが発生
+    """
+    from jra_tools import load
+
+    try:
+        kaisais = load(int(f"{start}0101"), int(f"{end}1231"))
+        ic = InputCreator(kaisais)
+        period = "data" if start == 2012 and end == 2018 else f"{start}_{end}"
+        ic.save(f"x_{period}.dump")
+    except Exception as e:
+        raise RuntimeError(
+            "トレーニングの入力データの作成中にエラーが発生しました"
+        ) from e
