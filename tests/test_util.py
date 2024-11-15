8cf35f460b21d857e203e5f20327f72a0add33be
Yuma Iwasaki <requrd1989@gmail.com>
2024-11-15 07:18:44 +0000

    move fixture kaisais

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,29 +1,9 @@
 """utilのユニットテスト"""
 
-from typing import List
-import os
-import pickle
-import pytest
 from jra_tools.machine_learning import util
 
 
-KaisaiList = List[util.KaisaiData]
-
-
-@pytest.fixture
-def sample_kaisais() -> KaisaiList:
-    """開催データのフィクスチャ
-
-    Returns:
-        KaisaiList: ローカルからロードずみの開催データ一覧
-    """
-    file_path = os.path.join(os.path.dirname(__file__), "sample_kaisais.pkl")
-
-    with open(file_path, "rb") as f:
-        return pickle.load(f)
-
-
-def test_create_payout(sample_kaisais: KaisaiList):
+def test_create_payout(sample_kaisais):
     """KaisaiListから払戻金のndarrayが作成されていることを確認する
 
     Args:
