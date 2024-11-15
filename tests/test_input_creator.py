b3c11d8637750374079a59959fde06a7ffcba41c
Yuma Iwasaki <requrd1989@gmail.com>
2024-11-15 09:45:55 +0000

    fix test input creator

--- tests/test_input_creator.py
+++ tests/test_input_creator.py
@@ -1,14 +1,22 @@
 """InputCreatorのユニットテスト"""
 
+import os
+import numpy as np
 from jra_tools.machine_learning.icreator import ICreator
 from jra_tools import InputCreator
 
 
 def test_save(sample_kaisais):
-    """saveメソッドのユニットテスト
+    """saveメソッドのユニットテスト"""
+    try:
+        ic = InputCreator(sample_kaisais)
+        ic.save("testfile")
 
-    Args:
-        sample_kaisais (_type_): kaisaisフィクスチャ
-    """
-    ic = InputCreator(sample_kaisais)
-    assert isinstance(ic, ICreator)
+        assert isinstance(ic, ICreator)
+        with open("x_testfile.dump", "rb") as f:
+            data = np.load(f, allow_pickle=True)
+
+        assert len(data) == 24
+    finally:
+        if os.path.exists("x_testfile.dump"):
+            os.remove("x_testfile.dump")
