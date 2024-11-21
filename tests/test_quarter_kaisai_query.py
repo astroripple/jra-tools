acc598d03d69c57ca93341c59fdfc1077200f337
Yuma Iwasaki <requrd1989@gmail.com>
2024-11-21 08:31:42 +0000

    add assert

--- tests/test_quarter_kaisai_query.py
+++ tests/test_quarter_kaisai_query.py
@@ -11,9 +11,11 @@ def test_quarter_kaisai_query(mocker: MockerFixture):
     """
     mock_load = mocker.patch("jra_tools.machine_learning.kaisai_loader.load")
 
+    from jra_tools.machine_learning.kaisai_loader import IQuery
     from jra_tools.machine_learning.quarter_kaisai_query import QuarterKaisaiQuery
 
     query = QuarterKaisaiQuery(2000, 2)
     query.load()
 
+    assert isinstance(query, IQuery)
     mock_load.assert_called_once_with(20000401, 20000630)
