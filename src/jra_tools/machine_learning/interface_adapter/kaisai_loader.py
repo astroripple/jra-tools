4004c509e0afb43d2a4709acbec872e31f6ed388
Yuma Iwasaki <requrd1989@gmail.com>
2024-11-22 09:41:05 +0000

    remove unnecessary protocol

--- src/jra_tools/machine_learning/interface_adapter/kaisai_loader.py
+++ src/jra_tools/machine_learning/interface_adapter/kaisai_loader.py
@@ -1,23 +1,9 @@
 """開催一覧を1年分以上ロードする"""
 
-from typing import List, Protocol, runtime_checkable
+from typing import List
 from dataclasses import dataclass
 from jrdb_model import KaisaiData
 from jra_tools.machine_learning.interface_adapter.iioloader import IOLoader
-
-
-@runtime_checkable
-class IQuery(Protocol):
-    @property
-    def period(self) -> str:
-        """期間を示すプロパティ"""
-
-    def load(self) -> List[KaisaiData]:
-        """開催データ一覧を取得する
-
-        Returns:
-            List[KaisaiData]: 開催データ一覧
-        """
 
 
 @dataclass
