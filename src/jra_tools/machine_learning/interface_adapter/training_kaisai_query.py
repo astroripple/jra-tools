fe57d8c007346d6b34856c73d2e4e0201f9f8eda
Yuma Iwasaki <requrd1989@gmail.com>
2024-11-22 09:10:54 +0000

    fix to use interface

--- src/jra_tools/machine_learning/interface_adapter/training_kaisai_query.py
+++ src/jra_tools/machine_learning/interface_adapter/training_kaisai_query.py
@@ -1,8 +1,10 @@
 """トレーニングデータ取得用クエリ"""
 
-from typing import List
+from typing import List, Callable
 from jrdb_model import KaisaiData
-from jra_tools.machine_learning.kaisai_loader import load
+from jra_tools.machine_learning.interface_adapter.ispecified_loader import (
+    SpecifiedLoader,
+)
 
 
 def _validate_year(year: int):
@@ -13,15 +15,21 @@ def _validate_year(year: int):
 class TrainingKaisaiQuery:
     """トレーニングデータ用クエリ"""
 
-    def __init__(self, start: int, end: int):
+    def __init__(
+        self,
+        start: int,
+        end: int,
+        loader_factory: Callable[[int, int], SpecifiedLoader],
+    ):
         _validate_year(start)
         _validate_year(end)
         assert start <= end, "終了年は開始年以降にしてください"
         self.start = start
         self.end = end
+        self.loader = loader_factory(self._start, self._end)
 
     def load(self) -> List[KaisaiData]:
-        return load(self._start, self._end)
+        return self.load()
 
     @property
     def _start(self):
