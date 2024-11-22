3d295e3bc9c84cf7b0a1a96f365bdeb9abac03fa
Yuma Iwasaki <requrd1989@gmail.com>
2024-11-22 09:24:55 +0000

    Fix to use query

--- src/jra_tools/machine_learning/usecase/iquery.py
+++ src/jra_tools/machine_learning/usecase/iquery.py
@@ -1,4 +1,4 @@
-"""IOロードのインターフェース"""
+"""クエリのインターフェース"""
 
 from typing import List, Protocol, runtime_checkable
 from jrdb_model import KaisaiData
@@ -6,7 +6,11 @@ from jrdb_model import KaisaiData
 
 @runtime_checkable
 class IQuery(Protocol):
-    """IOロードのインターフェース"""
+    """クエリのインターフェース"""
+
+    @property
+    def period(self) -> str:
+        """取得する期間"""
 
     def load(self) -> List[KaisaiData]:
         """開催データ一覧を取得する
