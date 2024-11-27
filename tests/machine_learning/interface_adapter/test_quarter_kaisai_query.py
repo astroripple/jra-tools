14d66959be2efc577644fe4b5626b7d3e1614ce6
Yuma Iwasaki <requrd1989@gmail.com>
2024-11-27 09:46:26 +0000

    Fix to use interface

--- tests/machine_learning/interface_adapter/test_quarter_kaisai_query.py
+++ tests/machine_learning/interface_adapter/test_quarter_kaisai_query.py
@@ -1,6 +1,19 @@
 """四半期用クエリのテスト"""
 
+from typing import Callable
+from dataclasses import dataclass
 from pytest_mock import MockerFixture
+
+
+@dataclass
+class MockSpecifiedLoader:
+    start: int
+    end: int
+    load: Callable
+
+    @property
+    def period(self) -> str:
+        return "sample_period"
 
 
 def test_quarter_kaisai_query(mocker: MockerFixture):
@@ -9,13 +22,20 @@ def test_quarter_kaisai_query(mocker: MockerFixture):
     Args:
         mocker (MockerFixture): _description_
     """
-    mock_load = mocker.patch("jra_tools.machine_learning.kaisai_loader.load")
+    from jra_tools.machine_learning.usecase.iquery import (
+        IQuery,
+    )
+    from jra_tools.machine_learning.interface_adapter.quarter_kaisai_query import (
+        QuarterKaisaiQuery,
+    )
 
-    from jra_tools.machine_learning.kaisai_loader import IQuery
-    from jra_tools.machine_learning.quarter_kaisai_query import QuarterKaisaiQuery
+    mock_loader_factory = mocker.patch("test_quarter_kaisai_query.MockSpecifiedLoader")
+    mock_loader = mock_loader_factory.return_value
+    mock_loader.load.assert_not_called()
 
-    query = QuarterKaisaiQuery(2000, 2)
+    query = QuarterKaisaiQuery(2000, 2, MockSpecifiedLoader)
     query.load()
 
     assert isinstance(query, IQuery)
-    mock_load.assert_called_once_with(20000401, 20000630)
+    mock_loader_factory.assert_called_once_with(20000401, 20000630)
+    mock_loader.load.assert_called_once_with()
