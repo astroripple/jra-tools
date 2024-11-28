9f57690f5e7b51cec31993fca82d2fb0a2db04fa
Yuma Iwasaki <requrd1989@gmail.com>
2024-11-28 07:28:02 +0000

    add test mysql loader

--- tests/machine_learning/infrastructure/test_mysql_loader.py
+++ tests/machine_learning/infrastructure/test_mysql_loader.py
@@ -1,5 +1,17 @@
 """MysqlLoaderのユニットテスト"""
 
+from pytest_mock import MockerFixture
 
-def test_mysql_loader():
-    pass
+
+def test_mysql_loader(mocker: MockerFixture):
+    mock_get_kaisais = mocker.patch(
+        "jra_tools.machine_learning.infrastructure.mysql_loader.get_kaisais"
+    )
+
+    from jra_tools.machine_learning.infrastructure.mysql_loader import MysqlLoader
+
+    loader = MysqlLoader(1, 1)
+    entities = loader.load()
+
+    assert entities == []
+    mock_get_kaisais.assert_called_once_with()
