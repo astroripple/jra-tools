5c34274a7137e5401b3dda64bfad6e5f0fdd9092
Yuma Iwasaki <requrd1989@gmail.com>
2024-11-28 09:00:06 +0000

    add test for score data

---
+++ tests/machine_learning/usecase/test_training_tool.py
@@ -0,0 +1,15 @@
+from typing import List
+from jrdb_model import KaisaiData
+from jra_tools.machine_learning.usecase.training_tool import create_score_data_matrix
+
+
+def test_create_score_data_matrix(sample_kaisais: List[KaisaiData]):
+    """開催データから数値データを作成するテスト
+
+    Args:
+        sample_kaisais (List[KaisaiData]): 開催一覧
+    """
+
+    scores = create_score_data_matrix(sample_kaisais)
+
+    assert scores.shape == (24, 18, 47)
