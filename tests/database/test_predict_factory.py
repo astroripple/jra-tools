9bab9d4d60aeb6487e2898ab05b0c6dc9783bb7a
requrd <requrd1989@gmail.com>
2025-02-07 04:41:31 +0000

    fix pydoc

--- tests/database/test_predict_factory.py
+++ tests/database/test_predict_factory.py
@@ -1,9 +1,11 @@
-"""モデルが推論可能なことをテストする"""
+"""モデルが推論可能なことをテストする."""
+
 from typing import List
+
 from jrdb_model import KaisaiData
+
 
 def test_predict(sample_kaisais: List[KaisaiData]):
     from jra_tools.database.predict_factory import PredictFactory
-    
-    factory = PredictFactory(,)
-    
+
+    factory = PredictFactory(sample_kaisais, 1)
