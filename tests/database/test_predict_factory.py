707975f1ab155a632ed5dab6d594d593da68b42e
requrd <requrd1989@gmail.com>
2025-02-07 05:04:34 +0000

    add fixture model

--- tests/database/test_predict_factory.py
+++ tests/database/test_predict_factory.py
@@ -1,11 +1,17 @@
 """モデルが推論可能なことをテストする."""
 
+from pathlib import Path
 from typing import List
 
+import keras
 from jrdb_model import KaisaiData
 
 
 def test_predict(sample_kaisais: List[KaisaiData]):
     from jra_tools.database.predict_factory import PredictFactory
 
-    factory = PredictFactory(sample_kaisais, 1)
+    # keras v2の既存モデルを暫定的にフィクスチャーにしている。
+    model_path = Path(__file__).parent / "ResNet_4blocks_20130101_20191231"
+    layer = keras.layers.TFSMLayer(model_path, call_endpoint="serving_default")
+
+    factory = PredictFactory(sample_kaisais, keras.Sequential([layer]))
