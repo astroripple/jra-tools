9c86488287ffc79ef953d55686f604551b5b90eb
requrd <requrd1989@gmail.com>
2025-02-07 04:26:14 +0000

    fix pydoc

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
-"""ユニットテストで共通利用するフィクスチャー"""
+"""ユニットテストで共通利用するフィクスチャー."""
 
-from typing import List
 import os
 import pickle
+from typing import List
+
 import pytest
 from jrdb_model import KaisaiData
 
@@ -11,10 +12,11 @@ KaisaiList = List[KaisaiData]
 
 @pytest.fixture
 def sample_kaisais() -> KaisaiList:
-    """開催データのフィクスチャ
+    """開催データのフィクスチャ.
 
     Returns:
         KaisaiList: ローカルからロードずみの開催データ一覧
+
     """
     file_path = os.path.join(os.path.dirname(__file__), "sample_kaisais.pkl")
 
