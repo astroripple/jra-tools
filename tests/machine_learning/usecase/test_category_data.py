72987cf2978b16a93bb5a888f91b5f325bd33920
requrd <requrd1989@gmail.com>
2025-02-07 02:58:56 +0000

    fix pydoc

--- tests/machine_learning/usecase/test_category_data.py
+++ tests/machine_learning/usecase/test_category_data.py
@@ -1,17 +1,19 @@
-"""カテゴリデータ取得のユニットテスト"""
+"""カテゴリデータ取得のユニットテスト."""
 
 from typing import List
+
 from jrdb_model import KaisaiData
+
 from jra_tools.machine_learning.usecase.category_data import get_category_data
 
 
 def test_get_category_data(sample_kaisais: List[KaisaiData]):
-    """カテゴリカルデータを取得するテスト
+    """カテゴリカルデータを取得するテスト.
 
     Args:
         sample_kaisais (List[KaisaiData]): 開催一覧
-    """
 
+    """
     categories = get_category_data(sample_kaisais)
 
-    assert categories.shape == (24, 18, 75)
+    assert categories.shape == (24, 18, 19)
