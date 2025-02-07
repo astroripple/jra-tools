ff3c4f73d329d505b2a0a916f721aecd09eb8c8e
requrd <requrd1989@gmail.com>
2025-02-07 04:40:00 +0000

    add test predict factory

---
+++ tests/database/test_predict_factory.py
@@ -0,0 +1,9 @@
+"""モデルが推論可能なことをテストする"""
+from typing import List
+from jrdb_model import KaisaiData
+
+def test_predict(sample_kaisais: List[KaisaiData]):
+    from jra_tools.database.predict_factory import PredictFactory
+    
+    factory = PredictFactory(,)
+    
